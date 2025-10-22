a0a8408e05b6abf0cc6b685ac2e906d616d904fb
william <espiw@farmingdale.edu>
2025-10-21 20:21:47 -0400

    Bro Code stopwatch imports

---
+++ pyqt5_sopwatch.py
@@ -0,0 +1,8 @@
+# python pyqt5 stopwatch
+
+# provides modules used and maintained by the python interpreter
+import sys
+# provides gui components
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
+# provides functionality
+from PyQt5.QtCore import QTimer, QTime, Qt
